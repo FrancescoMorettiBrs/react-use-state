91ebfbeab1623990d69887ac3e192e2e46f4bf2a
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-06-05 17:35:53 +0200

    feat: task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
-function App() {}
+import { useState } from "react";
+import languages from "../languages.js";
+const App = () => {
+  const [languageSelected, setLanguageSelected] = useState(languages[0]);
+
+  return (
+    <>
+      <div className="container">
+        <h1>Learn Web Development</h1>
+        {languages.map((language, index) => (
+          <button key={index} onClick={() => setLanguageSelected(language)} className={language.title === languageSelected.title ? "active-btn" : "btn"}>
+            {language.title}
+          </button>
+        ))}
+        <div className="paragraph">
+          <h3>{languageSelected.title}</h3>
+          <p>{languageSelected.description}</p>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default App;
